350ddad26af01203d0d4de05eaf9bc1ad58b7828
ttkk080128 <ttkk080128@yahoo.com.hk>
2021-11-06 08:08:44 +0800

    11.06 08.08

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-# -*- coding: MS950 -*-
-#載入LineBot所需要的套件
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*-  
+"""  
+@desc:  
+@author: TsungHan Yu  
+@contact: nick.yu@hzn.com.tw  
+@software: PyCharm  @since:python 3.6.0 on 2017/7/13
+"""
+
+import os
+import requests
 from flask import Flask, request, abort
 
 from linebot import (
@@ -8,18 +17,23 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
+"""
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+SECRET = os.environ.get('SECRET')
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(SECRET)
+"""
 line_bot_api = LineBotApi('dJGzSmMRuB3W1hYS7Q4Ljp7BZYjGOMzi71Gqml5iELw9rcaDGniyDZ0H235HHpLLBqCqJkcklCCZn/KvSeWyOUVfyZp5WMlsl30sY1pIlLacEFGXnthjHbdiOVB2bjZTuZ/k9QuFKF5qwMT2A8ie/QdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
 handler = WebhookHandler('2e6583026e25c1c4eff20500e5ed78f6')
 
-line_bot_api.push_message('1656595511', TextSendMessage(text='You can start now'))
 
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -37,14 +51,14 @@ def callback():
 
     return 'OK'
 
-#訊息傳遞區塊
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    res = event.message.text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=res))
 
-#主程式
-import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
